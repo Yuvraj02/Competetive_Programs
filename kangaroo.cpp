81dddaed642d93d69c3ab42c1c6095150272d506
Yuvraj02 <yuvrajsinghbhadoria@gmail.com>
2022-01-02 01:57:44 +0530

    Add files via upload

---
+++ kangaroo.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+bool lcm(int,int,int,int);
+
+
+
+int main(){
+
+    int x1,v1,x2,v2,k1_rate = x1+v1,k2_rate=x2+v2;
+
+   cin>>x1>>v1>>x2>>v2;
+   bool num = lcm(x1,v1,x2,v2);
+
+    if (v1>v2 || num)
+    {
+     while (k1_rate != k2_rate)
+        {
+        k1_rate += v1;
+        k2_rate += v2;
+        }
+        cout<<"YES";
+    }else{
+        cout<<"NO";
+    }    
+    
+    return 0;
+}
+
+bool lcm(int num1,int deno_1,int num2,int deno_2){
+
+    int result,num1_cal,num2_calc;
+
+    if(num1 == 0 )
+        return (num2%deno_2 ) == 0;
+
+    if (num2 ==0)
+    return (num1%deno_1)==0;
+
+    if (deno_1==deno_2)
+    {
+     return ((num1+num2)%deno_1) == 0;
+
+    } else if (deno_1%deno_2 == 0)
+    {
+        return ((num1 + (num2 * (deno_1/deno_2 * 1.0)))/deno_1) == 0;
+    
+    }else if(deno_2%deno_1 == 0){
+        return ((num2 + (num1 * (deno_2/deno_1 * 1.0)))/deno_2) == 0;
+    }
+    else { return 0;}
+}
